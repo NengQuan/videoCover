73ba00c67fe5d22b0200e49ac4d7c470382279b7
NengQuan <1020725009@qq.com>
2016-06-30 17:06:35 +0800

    Initial Commit

---
+++ videoCover/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  videoCover
+//
+//  Created by NengQuan on 16/6/30.
+//  Copyright © 2016年 NengQuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ videoCover/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  videoCover
+//
+//  Created by NengQuan on 16/6/30.
+//  Copyright © 2016年 NengQuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
